ac9044ccf1579c1cf6d9a4346ef35d291177577d
joshuatreeee <joshuatreeee@gmail.com>
2020-02-13 10:35:09 -0500

    added product view and controller, modified startup to see views

---
+++ CandyReview/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandyReview.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
