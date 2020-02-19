8f9b2d2f04e1b271c657a2de9628aec6b7ac8877
1robnor <robnorspeaks@gmail.com>
2020-02-19 11:51:25 -0500

    created connection string

---
+++ CandyReview/Data/CandyContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CandyReview.Models;
+
+namespace CandyReview.Data
+{
+    public class CandyContext : DbContext
+    {
+        public DbSet<ReviewModel> Reviews { get; set; } 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=CandyReview;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString)
+                .UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
