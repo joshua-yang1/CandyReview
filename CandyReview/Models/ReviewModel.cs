fa9a2455f4871a818bf5e5347024a4b207130332
1robnor <robnorspeaks@gmail.com>
2020-02-19 11:33:25 -0500

    added review model

---
+++ CandyReview/Models/ReviewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyReview.Models
+{
+    public class ReviewModel
+    {
+            public int Id { get; set; }
+            public string Content { get; set; }
+            public string Reviewer { get; set; }
+            public int Rating { get; set; }
+            public string Date { get; set; }
+
+        public ReviewModel()
+        {
+
+        }
+
+        public ReviewModel(int id, string content, string reviewer, int rating, string date)
+        {
+            Id = id;
+            Content = content;
+            Reviewer = reviewer;
+            Rating = rating;
+            Date = date;
+        }
+    }
+}
