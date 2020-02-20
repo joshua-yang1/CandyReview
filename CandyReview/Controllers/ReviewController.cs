08a2023fd360e05a188b03bf199feb736f199d56
1robnor <robnorspeaks@gmail.com>
2020-02-20 13:27:35 -0500

    created review controller class

---
+++ CandyReview/Controllers/ReviewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyReview.Models;
+using CandyReview.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandyReview.Controllers
+{
+    public class ReviewController : Controller
+    {
+        IRepository<ReviewModel> reviewRepo;
+
+        public ReviewController(IRepository<ReviewModel> reviewRepo)
+        {
+            this.reviewRepo = reviewRepo;
+        }
+
+        public ViewResult Index()
+        {
+            var model = reviewRepo.GetAll();
+
+            return View(model);
+        }
+
+    }
+}
