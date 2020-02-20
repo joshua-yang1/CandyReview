ec634488a69cc7865b4d23e3404a9c4a733ae906
1robnor <robnorspeaks@gmail.com>
2020-02-20 13:52:32 -0500

    added hard-coded reviews

--- CandyReview/Data/CandyContext.cs
+++ CandyReview/Data/CandyContext.cs
@@ -46,7 +46,70 @@ namespace CandyReview.Data
                     Category = "Chocolate"
                 });
 
+            ModelBuilder.Entity<ReviewModel>().HasData(
+
+                new ReviewModel()
+                {
+                    Id = 1,
+                    Content = "Review",
+                    Reviewer = "Corleeone",
+                    Rating = 3,
+                    Date = "july 32nd, 2020",
+                    ProductId = 1
+                },
+
+                  new ReviewModel()
+                  {
+                    Id = 2,
+                    Content = "Review",
+                    Reviewer = "Jake",
+                    Rating = 2,
+                    Date = "January 1st, 1893",
+                    ProductId = 1
+                  },
+
+                  new ReviewModel()
+                  {
+                    Id = 3,
+                    Content = "Review",
+                    Reviewer = "Jackie Fredrickson",
+                    Rating = 1,
+                    Date = "THE END TIMES",
+                    ProductId = 2
+                  },
+
+                  new ReviewModel()
+                  {
+                    Id = 4,
+                    Content = "Review",
+                    Reviewer = "Becky with the good hair",
+                    Rating = 47,
+                    Date = "33 BC",
+                    ProductId = 2
+                  },
+
+                  new ReviewModel()
+                  {
+                    Id = 5,
+                    Content = "Review",
+                    Reviewer = "Johnny Pearseed",
+                    Rating = 0,
+                    Date = "Day 1",
+                    ProductId = 3
+                  },
+
+                  new ReviewModel()
+                  {
+                    Id = 6,
+                    Content = "Review",
+                    Reviewer = "Harriet Tubman",
+                    Rating = 5,
+                    Date = "Decemberuary 4th, 3112",
+                    ProductId = 3
+                  }) ;
+
             base.OnModelCreating(modelBuilder);
+
 
         }
     }
